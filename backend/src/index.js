35f28780a59fe736f0b161c7e5ad579757d892b5
evanargenal <evan.argenal99@hotmail.com>
2025-02-10 16:29:30 -0700

    Added database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@database.gegel.mongodb.net/?retryWrites=true&w=majority&appName=database`;
+
+app.use(cors());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // const database = client.db('expense_tracker');
+    // const users = database.collection('users');
+    // const query = { name: 'Evan Argenal' };
+    // const name = await users.findOne(query);
+    // console.log(name);
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello from our server!')
-})
+  res.send('Hello from our server!');
+});
 
 app.listen(8080, () => {
-      console.log('server listening on port 8080')
-})
+  console.log('server listening on port 8080');
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+
+//data will be the string we send from our server
+const apiCall = () => {
+  axios.get('/').then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data);
+  });
+};
 
 function App() {
   return (
@@ -9,6 +20,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="primary" size="lg" onClick={apiCall}>
+          Make API Call
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
